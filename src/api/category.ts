bffe9a18dc0348754f4204743115ecfdc87c2d18
fit-s-u-m <nehemiahmaths@gmail.com>
2026-01-21 02:06:17 +0300

    added:  a way to get category id form name

--- src/api/category.ts
+++ src/api/category.ts
@@ -3,12 +3,20 @@ import { db } from "@/db";
 import { createCategory } from "@/validators/category"
 import { zValidator } from "@hono/zod-validator";
 import { category } from "@/db/schema"
+import { eq } from "drizzle-orm";
 
 const app = new Hono()
 
 app.get("/", async (c) => {
   const categories = await db.query.category.findMany()
   return c.json(categories)
+})
+
+app.get("/:category_name", async (c) => {
+  const { category_name } = c.req.param()
+  const categoryFinded = await db.query.category.findFirst({ where: eq(category.name, category_name) })
+  if (!categoryFinded) return c.json({ error: `can not find category with nam: ${category_name}` }, 400)
+  return c.json({ id: categoryFinded.id }, 200)
 })
 
 app.post("/", zValidator("json", createCategory), async (c) => {
